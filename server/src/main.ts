eae658371ff08f05642b65a17d750c5ef1856dee
Deivid Donchev <85338879+DNDonchev20@users.noreply.github.com>
2024-03-30 10:05:03 -0700

    Enable authentication

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS for all origins
+  app.enableCors(); 
 
   const config = new DocumentBuilder()
     .setTitle('API documentation')
