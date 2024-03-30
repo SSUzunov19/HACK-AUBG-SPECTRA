458583c930c1bbc3cd863005be528b5f1727257b
SSUzunov19 <SSUzunov19@codingburgas.bg>
2024-03-30 16:06:46 +0200

    Add rainbow

--- client/src/app/(routes)/page.tsx
+++ client/src/app/(routes)/page.tsx
@@ -1,9 +1,25 @@
-import Container from "@/components/ui/container";
+"use client";
 
-export default function Home() {
+import * as THREE from "three";
+import { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Rainbow } from "@/components/spectra/Rainbow";
+
+export default function App() {
   return (
-    <Container>
-      <></>
-    </Container>
+    <Canvas>
+      <color attach="background" args={["black"]} />
+      <Scene />
+    </Canvas>
   );
+}
+
+function Scene() {
+  const ref = useRef<THREE.Object3D>(null);
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.z += delta / 5;
+    }
+  });
+  return <Rainbow ref={ref} startRadius={0} endRadius={0.65} fade={0} />;
 }
