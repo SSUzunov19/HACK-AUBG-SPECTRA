cd4b36d0daa304bf2000a52500dbe50f2da31a37
SSUzunov19 <SSUzunov19@codingburgas.bg>
2024-03-30 14:39:25 +0200

    Setup project

---
+++ server/src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS for all origins
+
+  const config = new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
