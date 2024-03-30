a3fe3468a23adeb241f48994c963c8a8b3551e65
Kristian Milanov <54852657+kamilanov18@users.noreply.github.com>
2024-03-30 20:07:30 +0200

    Implement Home page

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        neue: ['"Neue Montreal"', "sans-serif"],
+        neueItalic: ['"Neue Montreal Italic"', "sans-serif"],
+      },
+      fontSize: {
+        "landing": "8rem",
       },
     },
   },
