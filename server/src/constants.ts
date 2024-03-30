a0434ff364cb72512ec1b7026b24efbeaa48be60
Deivid Donchev <85338879+DNDonchev20@users.noreply.github.com>
2024-03-30 15:52:42 -0700

    Change port

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,3 +1,5 @@
 export const jwtConstants = {
   secret: process.env.JWT_SECRET,
 };
+
+export const port: number = parseInt(process.env.PORT, 10);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
+import {port} from './constants';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); 
@@ -16,6 +18,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
